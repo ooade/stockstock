863a71bf2b015c0604cf3647a726d7fbea6e897a
Adegbuyi Ademola <marhyorh@gmail.com>
2015-12-15 06:57:15 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use('/bootstrap', express.static(path.join(__dirname, 'bower_components/bootstrap')));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
