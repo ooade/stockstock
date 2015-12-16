ca6695a2666d9866870379ce7878a28de9e2556b
Adegbuyi Ademola <marhyorh@gmail.com>
2015-12-16 09:15:27 +0100

    Implemented Socket.io

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use('/bootstrap', express.static(path.join(__dirname, 'bower_components/bootstrap')));
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -23,6 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname, 'bower_components/bootstrap')));
+app.use('/angular', express.static(path.join(__dirname, 'bower_components/angular')));
+app.use('/jquery', express.static(path.join(__dirname, 'bower_components/jquery')));
 
 app.use('/', routes);
 app.use('/users', users);
